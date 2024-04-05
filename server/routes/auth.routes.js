d878a44d32b72181ef0bea36d2ee695c2d2bbe4a
Zaid Khan <93735014+zaidk2021@users.noreply.github.com>
2024-04-06 01:02:01 +0530

    any commits

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,6 +6,6 @@ const router = express.Router()
 
 router.post("/register", register)
 router.post("/login", login)
-router.get("/logout", verifyToken, logout)
+router.get("/logout", logout)
 
 export default router
