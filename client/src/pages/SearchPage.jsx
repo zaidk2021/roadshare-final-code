c405cb9dbe6ad9d24a647b40869ed1d88b1fc64c
Zaid Khan <93735014+zaidk2021@users.noreply.github.com>
2024-04-07 17:54:49 +0530

    new

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import axios from 'axios';
 import MapTilerDisplay from '@/components/MapTilerDisplay';
